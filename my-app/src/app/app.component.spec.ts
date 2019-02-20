705a201f000d4335160cf018d767bd5a404ce3a2
Charles Williams <charlesjw94@outlook.com>
2019-02-19 22:30:38 -0500

    testing for failures

--- my-app/src/app/app.component.spec.ts
+++ my-app/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'my-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-app');
+    expect(app.title).toEqual('my-failed');
   });
 
   it('should render title in a h1 tag', () => {
