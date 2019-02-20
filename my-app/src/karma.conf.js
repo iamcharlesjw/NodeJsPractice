dcd063975003bda13b828c41faf48f0062f01c16
Charles Williams <charlesjw94@outlook.com>
2019-02-19 22:15:07 -0500

    setting up files for jenkins integration

--- my-app/src/karma.conf.js
+++ my-app/src/karma.conf.js
@@ -4,14 +4,14 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       //require('karma-chrome-launcher'),
       require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular/cli/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
