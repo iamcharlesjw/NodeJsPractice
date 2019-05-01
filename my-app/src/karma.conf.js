44fe179b128d0f0898b47d4f9f110344a0a4d347
Charles Williams <charlesjw94@outlook.com>
2019-04-30 23:59:38 -0400

    making some more modifications to the karma config file

--- my-app/src/karma.conf.js
+++ my-app/src/karma.conf.js
@@ -7,8 +7,8 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      //require('karma-chrome-launcher'),
-      require('karma-phantomjs-launcher'),
+      require('karma-chrome-launcher'),
+      //require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -36,7 +36,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeNoSandbox: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: true
   });
 };
