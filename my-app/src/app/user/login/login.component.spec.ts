4d442d5a8be1e5ef89feb86e6b1a505ae1e226dc
Charles Williams <charlesjw94@outlook.com>
2019-05-14 19:38:20 -0400

    resolving some additional test failures

--- my-app/src/app/user/login/login.component.spec.ts
+++ my-app/src/app/user/login/login.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { MaterialModule } from 'app/material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -10,7 +11,7 @@ describe('LoginComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
-      imports: [MaterialModule]
+      imports: [MaterialModule, BrowserAnimationsModule]
     })
     .compileComponents();
   }));
